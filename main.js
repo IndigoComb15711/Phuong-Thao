30eec8bd6b43bdfb15dc68800225e6e58b6b1f00
Nguyễn Thế Ngọc <nguyxnthngoc@gmail.com>
2024-06-19 07:24:03 +0700

    Update main.js

--- main.js
+++ main.js
@@ -8,8 +8,6 @@ function checkBirthday() {
     if (birthday.getDate() === today.getDate() && birthday.getMonth() === today.getMonth()) {
         message.textContent = `Chúc mừng sinh nhật, ${name}!`;
         message.style.color = 'green';
-    } else {
-        message.textContent = `Hôm nay không phải là sinh nhật của bạn, ${name}.`;
-        message.style.color = 'red';
+    
     }
 }
